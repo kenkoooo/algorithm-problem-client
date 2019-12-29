8e629f19c9617f86a523628e9f6b4af4f613e4a3
kenkoooo <kenkou.n@gmail.com>
2019-12-29 17:30:53 +0900

    Introduce async to AtCoderClient

--- src/atcoder/client.rs
+++ src/atcoder/client.rs
@@ -1,31 +1,27 @@
-use crate::util::HttpClient;
+use crate::util;
 use crate::Result;
 
 use super::*;
 
 const ATCODER_PREFIX: &str = "https://atcoder.jp";
 
-pub struct AtCoderClient {
-    http_client: HttpClient,
-}
+pub struct AtCoderClient;
 
 impl Default for AtCoderClient {
     fn default() -> Self {
-        Self {
-            http_client: HttpClient::default(),
-        }
+        Self
     }
 }
 
 impl AtCoderClient {
-    pub fn fetch_atcoder_contests(&self, page: u32) -> Result<Vec<AtCoderContest>> {
+    pub async fn fetch_atcoder_contests(&self, page: u32) -> Result<Vec<AtCoderContest>> {
         let url = format!("{}/contests/archive?lang=ja&page={}", ATCODER_PREFIX, page);
-        let html = self.http_client.get_html(&url)?;
+        let html = util::get_html(&url).await?;
         contest::scrape(&html)
     }
 
     /// Fetch a list of submissions.
-    pub fn fetch_atcoder_submission_list(
+    pub async fn fetch_atcoder_submission_list(
         &self,
         contest_id: &str,
         page: Option<u32>,
@@ -35,7 +31,7 @@ impl AtCoderClient {
             "{}/contests/{}/submissions?page={}",
             ATCODER_PREFIX, contest_id, page
         );
-        let html = self.http_client.get_html(&url)?;
+        let html = util::get_html(&url).await?;
         let submissions = submission::scrape(&html, contest_id)?;
         let max_page = submission::scrape_submission_page_count(&html)?;
         Ok(AtCoderSubmissionListResponse {
@@ -44,9 +40,9 @@ impl AtCoderClient {
         })
     }
 
-    pub fn fetch_problem_list(&self, contest_id: &str) -> Result<Vec<AtCoderProblem>> {
+    pub async fn fetch_problem_list(&self, contest_id: &str) -> Result<Vec<AtCoderProblem>> {
         let url = format!("{}/contests/{}/tasks", ATCODER_PREFIX, contest_id);
-        let html = self.http_client.get_html(&url)?;
+        let html = util::get_html(&url).await?;
         problem::scrape(&html, contest_id)
     }
 }
@@ -54,35 +50,36 @@ impl AtCoderClient {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use futures::executor::block_on;
 
     #[test]
     fn test_fetch_contest_list() {
         let client = AtCoderClient::default();
-        let contests = client.fetch_atcoder_contests(1).unwrap();
+        let contests = block_on(client.fetch_atcoder_contests(1)).unwrap();
         assert_eq!(contests.len(), 50);
     }
 
     #[test]
     fn test_fetch_problem_list() {
         let client = AtCoderClient::default();
-        let problems = client.fetch_problem_list("abc107").unwrap();
+        let problems = block_on(client.fetch_problem_list("abc107")).unwrap();
         assert_eq!(problems.len(), 4);
     }
 
     #[test]
     fn test_fetch_submission_list() {
         let client = AtCoderClient::default();
-        let response = client
-            .fetch_atcoder_submission_list("xmascon17", None)
-            .unwrap();
+        let response = block_on(client.fetch_atcoder_submission_list("xmascon17", None)).unwrap();
         assert_eq!(response.submissions.len(), 20);
 
-        let response = client
-            .fetch_atcoder_submission_list("xmascon17", Some(response.max_page))
-            .unwrap();
+        let response =
+            block_on(client.fetch_atcoder_submission_list("xmascon17", Some(response.max_page)))
+                .unwrap();
         assert!(!response.submissions.is_empty());
 
-        let response = client.fetch_atcoder_submission_list("xmascon17", Some(response.max_page + 1));
+        let response = block_on(
+            client.fetch_atcoder_submission_list("xmascon17", Some(response.max_page + 1)),
+        );
         assert!(response.is_err());
     }
 }
