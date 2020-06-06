03eb5d147a4332231882b2482cb278db9bc8a6a1
kenkoooo <kenkou.n@gmail.com>
2020-06-06 21:50:53 +0900

    Improve error types

--- src/error.rs
+++ src/error.rs
@@ -1,34 +1,15 @@
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-    Http(::surf::Exception),
+    #[error("connection error")]
+    Http(#[from] surf::Exception),
+    #[error("failed to parse html")]
     HtmlParseError,
-    ParseIntError(::std::num::ParseIntError),
-    ParseFloatError(::std::num::ParseFloatError),
-    ParseDateError(::chrono::format::ParseError),
+    #[error("failed to parse int")]
+    ParseIntError(#[from] std::num::ParseIntError),
+    #[error("failed to parse float")]
+    ParseFloatError(#[from] std::num::ParseFloatError),
+    #[error("failed to parse date")]
+    ParseDateError(#[from] chrono::format::ParseError),
 }
 
 pub type Result<T> = ::std::result::Result<T, Error>;
-
-impl From<::surf::Exception> for Error {
-    fn from(err: ::surf::Exception) -> Self {
-        Error::Http(err)
-    }
-}
-
-impl From<::std::num::ParseIntError> for Error {
-    fn from(err: ::std::num::ParseIntError) -> Self {
-        Error::ParseIntError(err)
-    }
-}
-
-impl From<::std::num::ParseFloatError> for Error {
-    fn from(err: ::std::num::ParseFloatError) -> Self {
-        Error::ParseFloatError(err)
-    }
-}
-
-impl From<::chrono::format::ParseError> for Error {
-    fn from(err: ::chrono::format::ParseError) -> Self {
-        Error::ParseDateError(err)
-    }
-}
